6e7929239912fc80ee794d0acb67207b4462b64c
gharshit2502 <gharshi2502@gmail.com>
2025-05-02 18:23:17 +0530

    Initial commit: TCET website clone

---
+++ app.js
@@ -0,0 +1,14 @@
+let data="Information of the Website"
+class user{
+    constructor(name,email){
+        this.name=name
+        this.email=email
+    }
+    viewdata(){
+        console.log("Website Data")
+
+    }
+}
+
+let s1=new user("abc","abc@mail.com")
+let s2= new user("xyz","xyz@mail.com")
